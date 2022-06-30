cd65b0a067e6116ab7a3d0aa7f29ba76996f2823
jack-leyland <jackjleyland@gmail.com>
2022-06-30 10:59:09 -0400

    maybe this?

--- models/Callframe.js
+++ models/Callframe.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const CallframeSchema = new mongoose.Schema({
     functionName: {type: String, required: false},
     scriptId: {type: Number, required: true},
-    bailoutReason: {type: String, required: false, default: ''},
+    bailoutReason: {type: String, required: false, default: ""},
     url: {type: String, required: false},
     lineNumber: {type: Number, required: true},
     columnNumber: {type: Number, required: true}
