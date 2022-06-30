468641f1d70adf7ade51ec2c44f8e99ece02f95f
jack-leyland <jackjleyland@gmail.com>
2022-06-30 10:47:05 -0400

    error handling fix

--- src/main/db.js
+++ src/main/db.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 
 // Need to add dynamic config for db connection rather than default everytime
 const conn = mongoose.createConnection('mongodb://localhost:27017/profiles')
-.catch((err) => {
-    console.log("DB CONNECTION ERROR: \n" + err);
-});
 
 mongoose.connection.on('error', err => {
-    console.log(err);
+    console.log("DB CONNECTION ERROR: \n" + err);
 })
+
+mongoose.connection.on('open', err => {
+    console.log("Profile DB connection successful");
+})
+
 
 // How to handle runtime errors with this connection object?
 
